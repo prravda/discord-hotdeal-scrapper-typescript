51b0eb3f908116f926ab8379835604d7c4509411
prravda <sonofhuman20@hufs.ac.kr>
2023-02-18 04:31:38 +0900

    fix: add error handler to try catch block

--- src/commands/slash/hotdeal-fmkorea.ts
+++ src/commands/slash/hotdeal-fmkorea.ts
@@ -11,51 +11,56 @@ export const HotDealFmKoreaCommand: SlashCommand = {
         ),
 
     execute: async (interaction) => {
-        await interaction.deferReply();
+        try {
+            await interaction.deferReply();
 
-        const scrapperInstance = new FmKoreaHotDealScrapper();
-        const hotDealResult = await scrapperInstance.requestDocument();
+            const scrapperInstance = new FmKoreaHotDealScrapper();
+            const hotDealResult = await scrapperInstance.requestDocument();
 
-        if (hotDealResult === undefined) {
-            throw Error('fmkorea hotdeal scrapper error');
-        }
+            if (hotDealResult === undefined) {
+                throw Error('fmkorea hotdeal scrapper error');
+            }
 
-        const resultEmbed = new EmbedBuilder()
-            .setColor(0xefff00)
-            .setTitle('펨코 핫 딜 목록!')
-            .setDescription('일반핫딜은 상품명/판매처/가격/배송비 입니다.')
-            .addFields(
-                { name: '------------', value: '------------' },
-                { name: '**인기핫딜**', value: '------' },
-                ...hotDealResult.popularHotDealList.map<APIEmbedField>(
-                    (deal) => {
-                        return {
-                            name: deal.title,
-                            value: deal.link,
-                        };
-                    }
+            const resultEmbed = new EmbedBuilder()
+                .setColor(0xefff00)
+                .setTitle('펨코 핫 딜 목록!')
+                .setDescription('일반핫딜은 상품명/판매처/가격/배송비 입니다.')
+                .addFields(
+                    { name: '------------', value: '------------' },
+                    { name: '**인기핫딜**', value: '------' },
+                    ...hotDealResult.popularHotDealList.map<APIEmbedField>(
+                        (deal) => {
+                            return {
+                                name: deal.title,
+                                value: deal.link,
+                            };
+                        }
+                    )
                 )
-            )
-            .addFields(
-                {
-                    name: '**일반핫딜**',
-                    value: '------',
-                },
-                ...hotDealResult.generalHotDealList.map<APIEmbedField>(
-                    (deal) => {
-                        return {
-                            name: `${deal.title} / ${deal.detailedInfo.sellerName} / ${deal.detailedInfo.productPrice} / ${deal.detailedInfo.shippingCharge}`,
-                            value: deal.link,
-                        };
-                    }
+                .addFields(
+                    {
+                        name: '**일반핫딜**',
+                        value: '------',
+                    },
+                    ...hotDealResult.generalHotDealList.map<APIEmbedField>(
+                        (deal) => {
+                            return {
+                                name: `${deal.title} / ${deal.detailedInfo.sellerName} / ${deal.detailedInfo.productPrice} / ${deal.detailedInfo.shippingCharge}`,
+                                value: deal.link,
+                            };
+                        }
+                    )
                 )
-            )
-            .setFooter({
-                text: '오류제보 및 기능개선은 #봇_기능_건의 혹은 prravda#8996 로',
-            });
+                .setFooter({
+                    text: '오류제보 및 기능개선은 #봇_기능_건의 혹은 prravda#8996 로',
+                });
 
-        if (hotDealResult) {
-            await interaction.editReply({ embeds: [resultEmbed] });
+            if (hotDealResult) {
+                await interaction.editReply({ embeds: [resultEmbed] });
+            }
+        } catch (e) {
+            console.error(e);
+            await interaction.editReply({ message: '오류 발생' });
         }
     },
 };
