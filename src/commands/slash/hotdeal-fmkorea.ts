8d2a9a732487ea5712f5fb2829b83e06485ae7ca
prravda <sonofhuman20@hufs.ac.kr>
2023-02-18 02:54:22 +0900

    refactor: segregate parsing logic by differnt concerns

--- src/commands/slash/hotdeal-fmkorea.ts
+++ src/commands/slash/hotdeal-fmkorea.ts
@@ -6,7 +6,9 @@ import { FmKoreaHotDealScrapper } from '../../scrappers/fmkorea-hot-deal-scrappe
 export const HotDealFmKoreaCommand: SlashCommand = {
     command: new SlashCommandBuilder()
         .setName('펨')
-        .setDescription(` 기준 유효한 펨코 핫딜을 불러옵니다.`),
+        .setDescription(
+            `펨코 핫딜을 불러옵니다. 갱신은 약 15분에 한 번씩 이뤄집니다.`
+        ),
 
     execute: async (interaction) => {
         await interaction.deferReply();
@@ -23,21 +25,25 @@ export const HotDealFmKoreaCommand: SlashCommand = {
             .setTitle('펨코 핫 딜 목록!')
             .setDescription('상품명/판매처/가격/배송비 순입니다.')
             .addFields(
-                ...hotDealResult.popularItemList.map<APIEmbedField>((deal) => {
-                    return {
-                        name: deal.title,
-                        value: deal.link,
-                        inline: true,
-                    };
-                })
+                ...hotDealResult.popularHotDealList.map<APIEmbedField>(
+                    (deal) => {
+                        return {
+                            name: deal.title,
+                            value: deal.link,
+                            inline: true,
+                        };
+                    }
+                )
             )
             .addFields(
-                ...hotDealResult.generalItemList.map<APIEmbedField>((deal) => {
-                    return {
-                        name: `${deal.title}/${deal.detailedInfo.sellerName}/${deal.detailedInfo.productPrice}/${deal.detailedInfo.shippingCharge}`,
-                        value: deal.link,
-                    };
-                })
+                ...hotDealResult.generalHotDealList.map<APIEmbedField>(
+                    (deal) => {
+                        return {
+                            name: `${deal.title}/${deal.detailedInfo.sellerName}/${deal.detailedInfo.productPrice}/${deal.detailedInfo.shippingCharge}`,
+                            value: deal.link,
+                        };
+                    }
+                )
             )
             .setFooter({
                 text: '오류제보 및 기능개선은 #봇_기능_건의 혹은 prravda#8996 로',
