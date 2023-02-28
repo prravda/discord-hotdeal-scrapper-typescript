7af8502a03ef393ba4b694a80e8f6ea85ef1fbba
prravda <sonofhuman20@hufs.ac.kr>
2023-02-28 11:14:03 +0900

    impl: separate command enrollment and client instance creating

--- infra/discord/command-handler.ts
+++ infra/discord/command-handler.ts
@@ -1,40 +1,11 @@
-import {
-    Command,
-    DiscordCommandEnrollResponse,
-    SlashCommand,
-} from '../../types';
-import {
-    Client,
-    Collection,
-    GatewayIntentBits,
-    REST,
-    Routes,
-} from 'discord.js';
+import { DiscordCommandEnrollResponse, SlashCommand } from '../../types';
+import { Collection, GatewayIntentBits, REST, Routes } from 'discord.js';
 import { envList } from '../env-config';
 
 export class CommandHandler {
-    constructor(
-        private readonly slashCommands: SlashCommand[],
-        private readonly generalCommands: Command[],
-        private readonly client: Client
-    ) {}
+    constructor(private readonly slashCommands: SlashCommand[]) {}
 
-    public enrollCommandsToLocalClient(): Client {
-        this.client.commands = new Collection();
-        this.client.slashCommands = new Collection<string, SlashCommand>();
-
-        this.slashCommands.forEach((c) => {
-            this.client.slashCommands.set(c.command.name, c);
-        });
-
-        this.generalCommands.forEach((c) => {
-            this.client.commands.set(c.name, c);
-        });
-
-        return this.client;
-    }
-
-    public async enrollCommandToDiscordInfra(): Promise<void> {
+    public async enrollCommandsToDiscordInfra(): Promise<void> {
         const restClient = new REST({
             version: envList.DISCORD_API_VERSION,
         }).setToken(envList.DISCORD_TOKEN);
@@ -56,6 +27,25 @@ export class CommandHandler {
             `);
         } catch (e) {
             console.error(e);
+            throw e;
+        }
+    }
+
+    public getCommandCollection() {
+        try {
+            const slashCommandCollection = new Collection<
+                string,
+                SlashCommand
+            >();
+
+            this.slashCommands.forEach((c) => {
+                slashCommandCollection.set(c.command.name, c);
+            });
+
+            return slashCommandCollection;
+        } catch (e) {
+            console.error(e);
+            throw e;
         }
     }
 }
