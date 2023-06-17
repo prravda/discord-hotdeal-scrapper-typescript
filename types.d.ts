55450cbbe727aba5135fcee7c4677b66db7ce6e2
prravda <sonofhuman20@hufs.ac.kr>
2023-06-17 21:36:45 +0900

    refactor: - removed discord-related types

--- types.d.ts
+++ types.d.ts
@@ -1,16 +1,3 @@
-import {
-    AutocompleteInteraction,
-    CommandInteraction,
-    SlashCommandBuilder,
-} from 'discord.js';
-
-export interface SlashCommand {
-    command: SlashCommandBuilder;
-    execute: (interaction: CommandInteraction) => Promise<void>;
-    autocomplete?: (interaction: AutocompleteInteraction) => void;
-    cooldown?: number;
-}
-
 export interface BasicHotDeal {
     id: number;
     title: string;
@@ -31,35 +18,4 @@ export interface FmKoreaGeneralHotDeal extends BasicHotDeal {
 export interface FmKoreaTotalHotDeal {
     general: FmKoreaGeneralHotDeal[];
     popular: FmKoreaPopularHotDeal[];
-}
-
-export interface DiscordCommandEnrollResponse {
-    id: string;
-    application_id: string;
-    version: string;
-    default_permission: boolean;
-    default_member_permissions: any;
-    type: number;
-    nsfw: boolean;
-    name: string;
-    name_localizations: any;
-    description: string;
-    description_localizations: any;
-    guild_id: string;
-}
-
-export interface Command {
-    name: string;
-    execute: (message: Message, args: Array<string>) => void;
-    permissions: Array<PermissionResolvable>;
-    aliases: Array<string>;
-    cooldown?: number;
-}
-
-declare module 'discord.js' {
-    export interface Client {
-        slashCommands: Collection<string, SlashCommand>;
-        commands: Collection<string, Command>;
-        cooldowns: Collection<string, number>;
-    }
 }
