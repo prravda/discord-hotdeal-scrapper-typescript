4937c79e60882f87e7a3f44b3c3db344f718537a
prravda <sonofhuman20@hufs.ac.kr>
2023-06-17 21:35:48 +0900

    refactor: - removed discord-related infra

--- src/schedulers/ppomppu-hot-deal-periodically.ts
+++ src/schedulers/ppomppu-hot-deal-periodically.ts
@@ -1,14 +1,50 @@
 import { PpomppuHotDealScrapper } from '../scrappers/ppomppu-hot-deal-scrapper';
+import { ClientInstance } from '../../infra/discord/client-instance';
+import { APIEmbedField, EmbedBuilder } from 'discord.js';
+import { envList } from '../../infra/env-config';
 
 export const ppomppuHotDealPeriodically = async () => {
     try {
+        const client = ClientInstance.getClient();
         const ppomppuScrapper = new PpomppuHotDealScrapper();
 
         const job = async () => {
             const refreshedDealList =
                 await ppomppuScrapper.getRefreshedHotDealList();
 
-            // TODO: send this hot deal into broker's producer
+            const hotDealBroadcastChannel = await client.channels.fetch(
+                envList.HOT_DEAL_CHANNEL_ID
+            );
+
+            if (
+                refreshedDealList &&
+                refreshedDealList.length > 0 &&
+                hotDealBroadcastChannel &&
+                hotDealBroadcastChannel.isTextBased()
+            ) {
+                const resultAsEmbed = new EmbedBuilder()
+                    .setColor(0xefff00)
+                    .setTitle('뽐뿌 핫 딜 목록!')
+                    .setDescription(
+                        `${new Date().toLocaleTimeString('ko-KR', {
+                            timeZone: 'Asia/Seoul',
+                        })} 기준 뽐뿌에서 갱신된 핫 딜 목록입니다.`
+                    )
+                    .addFields(
+                        ...refreshedDealList.map<APIEmbedField>((deal) => {
+                            return {
+                                name: `**· ${deal.title}**`,
+                                value: `[└─해당 핫 딜 바로가기(클릭)](${deal.link})`,
+                            };
+                        })
+                    )
+                    .setFooter({
+                        text: '오류제보 및 기능개선은 #봇_기능_건의 혹은 prravda#8996 로',
+                    });
+                await hotDealBroadcastChannel.send({
+                    embeds: [resultAsEmbed],
+                });
+            }
 
             setTimeout(
                 job,
