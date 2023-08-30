cab9a801f4d6717d1895337b7bb3726e6689a93c
prravda <sonofhuman20@hufs.ac.kr>
2023-08-31 04:14:56 +0900

    cleanup: - uninstalled pyroscope and it's related component

--- app.ts
+++ app.ts
@@ -1,16 +1,8 @@
 import { ppomppuHotDealPeriodically } from './src/schedulers/ppomppu-hot-deal-periodically';
 import { fmKoreaHotDealPeriodically } from './src/schedulers/fmkorea-hot-deal-periodically';
 import { BrokerPublisher } from './infra/broker/broker-publisher';
-import Pyroscope from '@pyroscope/nodejs';
 
 async function bootstrap() {
-    // run profiler
-    Pyroscope.init({
-        serverAddress: 'http://pyroscope:4040',
-        appName: 'hotdeal-scrapper',
-    });
-    Pyroscope.start();
-
     // start broker - publisher
     await BrokerPublisher.start();
 
